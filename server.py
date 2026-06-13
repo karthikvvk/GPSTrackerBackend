eb7669d29df14162d6da19c54d0fb191bc6212ca
karthikvvk <vkarthik5002@gmail.com>
2026-06-13 17:46:18 +0530

    port no

--- server.py
+++ server.py
@@ -567,6 +567,6 @@ def handle_parent_unsubscribe(data):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 3000))
     print(port)
     socketio.run(app, host='0.0.0.0', port=port)
